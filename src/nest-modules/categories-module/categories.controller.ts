b18cf08d1844c7798b000b776b522337764ae8e1
David Maisonnette Jentjens <davidjentjens@gmail.com>
2024-01-08 16:13:29 -0300

    implement create category operation

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -6,17 +6,40 @@ import {
     Patch,
     Param,
     Delete,
+    Inject,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
+import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
+import { DeleteCategoryUseCase } from '../../core/category/application/use-cases/delete-category/delete-category.use-case';
+import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
 
 @Controller('categories')
 export class CategoriesController {
-    constructor(private categoryRepo: CategorySequelizeRepository) {}
+    // NOTE: The two ways of injecting dependencies are equivalent, but the second one is more concise, since the class
+    // name is already registered in the container.
+    // @Inject(CATEGORY_PROVIDERS.USE_CASES.CREATE_CATEGORY_USE_CASE)
+    @Inject(CreateCategoryUseCase)
+    private createUseCase: CreateCategoryUseCase;
+
+    @Inject(UpdateCategoryUseCase)
+    private updateUseCase: UpdateCategoryUseCase;
+
+    @Inject(DeleteCategoryUseCase)
+    private deleteUseCase: DeleteCategoryUseCase;
+
+    @Inject(GetCategoryUseCase)
+    private getUseCase: GetCategoryUseCase;
+
+    @Inject(ListCategoriesUseCase)
+    private listUseCase: ListCategoriesUseCase;
 
     @Post()
-    create(@Body() createCategoryDto: CreateCategoryDto) {}
+    create(@Body() createCategoryDto: CreateCategoryDto) {
+        return this.createUseCase.execute(createCategoryDto);
+    }
 
     @Get()
     findAll() {}
